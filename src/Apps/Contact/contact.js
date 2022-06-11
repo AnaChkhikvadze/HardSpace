d7cf2b71591773f2b5b4783b41c7e5d4e71d1da5
kiu_chkhikvadze.ana <chkhikvadze.ana@kiu.edu.ge>
2022-06-11 22:18:53 +0400

    my first commit

---
+++ src/Apps/Contact/contact.js
@@ -0,0 +1,24 @@
+// import React from 'react'
+// import NewsletterSubscribe from './NewsletterSubscripe/NewsletterSubscribe'
+
+// export default function contact() {
+//     return (
+//         // <div><NewsletterSubscribe /></div>
+//         <form action="" inSubmit="">
+//             <div className='inputsCont'>
+//                 <h2>Say Hello!</h2>
+//                 <span>Full Name</span>
+//                 <input className='inputContact' type='text' name='fullname' required></input>
+//                 <span>Phone Number</span>
+//                 <input className='inputContact' type='text' name='phone' required></input>
+//                 <span>Email</span>
+//                 <input className='inputContact' type='text' name='email' required></input>
+//             </div>
+//             <div className='formCont'>
+//                 <span>Message</span>
+//                 <textarea name="message" required></textarea>
+//                 <button>Submit</button>
+//             </div>
+//         </form>
+//     )
+// }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { FaBars, FaTimes } from 'react-icons/fa'
 import './NavbarStyle.css'
+import {Link} from 'react-router-dom'
 
 const Navbar = () => {
     const [click, setClick] = useState(false)
@@ -11,20 +12,27 @@ const Navbar = () => {
         <div className='Navbar'>
             <div className='nav-container'>
                 <a href="#">
-                    <h2><span style={{ color: '#112d32' }}>Hard</span><span style={{ color: '#88bdbc' }}>Space</span></h2>
+                    <h2><Link to='/'><span style={{ color: '#112d32' }}>Hard</span><span style={{ color: '#88bdbc' }}>Space</span></Link></h2>
                 </a>
                 <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                     <li>
-                        <a href='/'>Home</a>
+                        <a>
+                    <Link to='/' className='homeNav'>Home</Link>
+                    </a>
                     </li>
                     <li>
-                        <a href='/'>Destinations</a>
+                    <a>
+                    <Link to='/destinations' className='navLink'>Destinations</Link>
+                    </a>
                     </li>
                     <li>
-                        <a href='/'>Blogs</a>
+                    <Link to='/blogs' className='navLink'>Blogs</Link>
                     </li>
                     <li>
-                        <a href='/'>About Us</a>
+                        <Link to='/about' className='navLink'>About</Link>
+                    </li>
+                    <li>
+                        <Link to='/contact' className='navLink'>Contact</Link>
                     </li>
                 </ul>
                 <button className='nav-signup'>Account</button>
